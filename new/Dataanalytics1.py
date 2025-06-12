6328c497144cb5d3e73feb1a6a955a33a8d4b284
Favour Obisike Oluebubechi <chikkyfavour1@gmail.com>
2025-06-12 17:43:39 +0100

    New Code

--- new/Dataanalytics1.py
+++ new/Dataanalytics1.py
@@ -1,36 +1,3 @@
-'''
-import requests
-import urllib.request as k
-
-API_KEY1 = "a53cdae913a5e85c56ee542b33bedccd"
-API_KEY2 = "8cc318f610b994a0058803f7f8e27ee1"
-CITY = "London"
-BASE_URL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=a53cdae913a5e85c56ee542b33bedccd"
-
-params = {
-    "q": CITY,
-    "appid": API_KEY1,
-    "units": "metric"  # Use 'imperial' for Fahrenheit
-}
-
-response = requests.get(BASE_URL, params=params)
-
-
-data = response.json()
-for location in data:
-    name = location.get("name")
-    lat = location.get("lat")
-    lon = location.get("lon")
-    
-    
-    #print(f"{name}: Latitude = {lat}, Longitude = {lon}")
-
-path = f"api.openweathermap.org/data/2.5/forecast?{lat}&{lon}&appid={API_KEY2}"
-reply = k.urlopen(path,str,1000)
-data1 = reply.json()
-'''   
-
-
 import requests
 
 def get_weather_openweathermap(city, api_key):
